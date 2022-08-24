80ea527b04b636d793fa7a48aa730aace856d416
Abdulmalik-rebdi <Abdulmalik.rebdi@gmail.com>
2022-08-24 03:34:59 +0000

    ordering in app.py

--- LR/app.py
+++ LR/app.py
@@ -16,14 +16,15 @@ user_id  = st.text_input('write your user id if you have one' , placeholder  = "
 len_of_title  = st.text_input("write the title you want to pu in the site " , placeholder  = 'شقة للإيجار في شارع إبراهيم بن الأحمدي')
 len_of_content = st.text_area('enter the content that you want to put in the site ') 
 img_in_post = st.slider('how many image you want to put  ', 1, 30, 8)
-price  = st.number_input("Enter price you want to  ")
+age =    st.slider('how old is your apartment  note that 0 indicate new', 0, 40, 2)
 beds =  st.slider('how many bedrooms does your apartment have ', 1, 7, 1)
 livings = st.slider('how many living room does your apartment have ', 0, 5, 1)
 wc =    st.slider('how many water closet  does your apartment have ', 0, 5, 1)
 area = st.number_input("what is the area of the apartment  ")
 street_width = st.number_input("what is the street width of the apartment  ")
 # age = st.number_input("how old is your apartment  " )
-age =    st.slider('how old is your apartment  note that 0 indicate new', 0, 40, 2)
+price  = st.number_input("Enter price you want to  ")
+
 ketchen1 = st.radio(
      "does the apartment has  ketchen ",
      ('YES', 'NO'))
@@ -44,13 +45,7 @@ if furnished1 == 'YES':
     furnished = 1 
 else :
     furnished = 0 
-district = st.selectbox(
-     'How would you like to be contacted?',
-     (L1))
-advertiser_type = st.selectbox(
-     'what is your status in the site ',
-     (L2))
-review =   st.number_input("your review in the site  " , min_value=1 , max_value=5)
+
 profileImg1 =  st.radio(
      "do you have profile image ",
      ('YES', 'NO'))
@@ -68,7 +63,13 @@ else :
 zone =  st.selectbox(
      'what is the location of  the apartment ',
      (L3))
-
+district = st.selectbox(
+     'How would you like to be contacted?',
+     (L1))
+advertiser_type = st.selectbox(
+     'what is your status in the site ',
+     (L2))
+review =   st.number_input("your review in the site  " , min_value=1 , max_value=5)
 # createYEAR = 
 # createMONTH  = 
 # createDAY = 
@@ -87,10 +88,10 @@ if st.button("Submit"):
     prediction = clf.predict(X)[0]
     if prediction == True:
         clf1 = joblib.load("./src/reg.pkl")
-        prediction = clf1.predict(X)[0]
-        round(prediction)
+        prediction1 = clf1.predict(X)[0]
+        round(prediction1)
     # Output prediction
     # for col in X:
-        st.text(f"This apartment will be rented in {round(prediction)} days")
+        st.text(f"This apartment will be rented in {round(prediction1)} days")
     else:
         st.text(f"This apartment will not be rented ")
