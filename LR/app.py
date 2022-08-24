32953c46477250469db83fc9aad643de0d309a11
Abdulmalik-rebdi <Abdulmalik.rebdi@gmail.com>
2022-08-24 03:49:47 +0000

    review

--- LR/app.py
+++ LR/app.py
@@ -1,3 +1,4 @@
+from curses import flash
 import streamlit as st
 import pandas as pd
 import joblib
@@ -13,26 +14,26 @@ user_id  = st.text_input('write your user id if you have one' , placeholder  = "
 # txt = st.text_area('Text to analyze')
 # st.write('Sentiment:', run_sentiment_analysis(txt))
 # height = st.slider("Enter Height", 75, 95, 75)
-len_of_title  = st.text_input("write the title you want to pu in the site " , placeholder  = 'شقة للإيجار في شارع إبراهيم بن الأحمدي')
-len_of_content = st.text_area('enter the content that you want to put in the site ') 
-img_in_post = st.slider('how many image you want to put  ', 1, 30, 8)
-age =    st.slider('how old is your apartment  note that 0 indicate new', 0, 40, 2)
+len_of_title  = st.text_input("write the title of the post " , placeholder  = 'شقة للإيجار في شارع إبراهيم بن الأحمدي')
+len_of_content = st.text_area('enter the content of the post ') 
+img_in_post = st.slider('how many images in the post', 1, 30, 8)
+age =    st.slider('how old is your apartment  \nnote that 0 indicate new', 0, 40, 2)
 beds =  st.slider('how many bedrooms does your apartment have ', 1, 7, 1)
 livings = st.slider('how many living room does your apartment have ', 0, 5, 1)
 wc =    st.slider('how many water closet  does your apartment have ', 0, 5, 1)
 area = st.number_input("what is the area of the apartment  ")
 street_width = st.number_input("what is the street width of the apartment  ")
 # age = st.number_input("how old is your apartment  " )
-price  = st.number_input("Enter price you want to  ")
+price  = st.number_input("Enter price of the apartment ")
 
 ketchen1 = st.radio(
-     "does the apartment has  ketchen ",
+     "does the apartment has  a kitchen ",
      ('YES', 'NO'))
 if ketchen1 == 'YES':
     ketchen = 1 
 else :
     ketchen = 0 
-ac1 = st.radio("does the apartment has  ac ON ",
+ac1 = st.radio("does the apartment has  come with ac  ",
      ('YES', 'NO'))
 if ac1 == 'YES':
     ac = 1 
@@ -54,7 +55,7 @@ if profileImg1 == 'YES':
 else :
     profileImg = 0 
 iam_verified1  =   st.radio(
-     "are you varified user ",
+     "are you varified user in the site ",
      ('YES', 'NO'))
 if iam_verified1 == 'YES':
     iam_verified = 1 
@@ -64,12 +65,12 @@ zone =  st.selectbox(
      'what is the location of  the apartment ',
      (L3))
 district = st.selectbox(
-     'How would you like to be contacted?',
+     'what is the district of  the apartment',
      (L1))
 advertiser_type = st.selectbox(
      'what is your status in the site ',
      (L2))
-review =   st.number_input("your review in the site  " , min_value=1 , max_value=5)
+review =   st.number_input("your review in the site  " , value = 1.00 , step =0.1 ,min_value=1.00 , max_value=5.00   )
 # createYEAR = 
 # createMONTH  = 
 # createDAY = 
